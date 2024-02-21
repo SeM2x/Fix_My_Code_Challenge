3e68851761b9059cd2391eec1eb3958b35c9f48d
SeM2x <houssem135.302@gmail.com>
2024-02-21 17:01:42 +0100

    2-sort.rb

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -17,11 +17,10 @@ ARGV.each do |arg|
     i = 0
     l = result.size
     while !is_inserted && i < l do
-        is_inserted = false
         if result[i] < i_arg
             i += 1
         else
-            result.insert(i - 1, i_arg)
+            result.insert(i, i_arg)
             is_inserted = true
             break
         end
